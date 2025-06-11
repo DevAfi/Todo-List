cb6c61ccadc0bf8ae7d8eb66b7d571fb4ce6db2c
Afonso Carvalho <afonsomiguelcarvalho2006@gmail.com>
2025-06-12 00:56:58 +0100

    finished styling and added JavaScript

---
+++ ToDo-List/taskScript.js
@@ -0,0 +1,38 @@
+const inputField = document.getElementById("inputBox");
+const mainListContainer = document.getElementById("actualListID");
+
+function addTask(){
+    if (inputField.value.trim() === "") {
+        alert("Cannot pass an empty task");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputField.value;
+        mainListContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00D7"; 
+        li.appendChild(span);
+        
+    }
+    inputField.value = "";
+    saveItems();
+}
+
+mainListContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checkedItem");
+        saveItems();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveItems();
+    }
+}, false);
+
+function displayTasks() {
+    mainListContainer.innerHTML = localStorage.getItem("data");
+}
+
+function saveItems() {
+    localStorage.setItem("data", mainListContainer.innerHTML);
+}
+displayTasks();
